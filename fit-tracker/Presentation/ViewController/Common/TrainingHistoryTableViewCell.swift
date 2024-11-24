fca480204a9db43738065fcdf2969d9741e0375b
Hayato Ogura <hayato_ogura@r.recruit.co.jp>
2024-11-24 15:57:04 +0900

    途中経過

--- fit-tracker/Presentation/ViewController/Common/TrainingHistoryTableViewCell.swift
+++ fit-tracker/Presentation/ViewController/Common/TrainingHistoryTableViewCell.swift
@@ -8,7 +8,44 @@
 import UIKit
 
 class TrainingHistoryTableViewCell: UITableViewCell, GetClassNameProtocol {
-
+    
+    // 部位表示用View
+    @IBOutlet weak var trainingPartView: UIView!
+    // 部位
+    @IBOutlet weak var trainigPartLabel: UILabel!
+    // メニュー名
+    @IBOutlet weak var menuName: UILabel!
+    // セット情報
+    @IBOutlet weak var setData: UILabel!
+    
+    /// トレーニング履歴セル種別
+    enum CellType {
+        /// 部位見出しあり、メニュー名あり
+        case withBodyPart
+        /// 部位見出しなし、メニュー名あり
+        case withMenuName
+        /// 部位見出しなし、メニュー名なし
+        case withoutMenuName
+    }
+    
+    /// トレーニング履歴セル用のViewModel
+    struct TrainingHistoryCellModel {
+        /// 部位
+        let part: String
+        /// メニュー名
+        let menuName: String
+        /// 重さ
+        private let weight: Double
+        /// 回数
+        private let rep: Int
+        /// セット情報（表示用）
+        var setData: String {
+            return "\(weight)kg × \(rep)reps"
+        }
+        /// トレーニング履歴セル種別
+        let cellType: CellType
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -18,6 +55,22 @@ class TrainingHistoryTableViewCell: UITableViewCell, GetClassNameProtocol {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func configureCell(cellModel: TrainingHistoryCellModel) {
+        
+        switch cellModel.cellType {
+        case .withBodyPart:
+            self.trainigPartLabel.text = cellModel.part
+            self.menuName.text = cellModel.menuName
+        case .withMenuName:
+            self.trainingPartView.isHidden = true
+            self.menuName.text = cellModel.menuName
+        case .withoutMenuName:
+            self.trainigPartLabel.isHidden = true
+            self.menuName.isHidden = true
+        }
+        self.setData.text = cellModel.setData
     }
     
 }
--- fit-tracker/Presentation/ViewController/Home/HomeViewController.swift
+++ fit-tracker/Presentation/ViewController/Home/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 class HomeViewController: UIViewController, GetClassNameProtocol {
     
@@ -18,6 +19,54 @@ class HomeViewController: UIViewController, GetClassNameProtocol {
         super.viewDidLoad()
         
         configureNavBar()
+        // Mockデータセット
+        createMockData()
+        
+        configureTableView()
+    }
+    
+    // TODO: 消す
+    private func createMockData() {
+        
+        let history1 = TrainingHistory(
+            part: "胸",
+            trainingMenuList: [
+                TrainingHistory.TrainingMenu(
+                    trainingMenuName: "ベンチプレス",
+                    trainingSetList: [
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 70.0, rep: 8),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 65.0, rep: 8),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 60.0, rep: 12),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 60.0, rep: 10),
+                    ]
+                ),
+                TrainingHistory.TrainingMenu(
+                    trainingMenuName: "インクラインプレス",
+                    trainingSetList: [
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 65.0, rep: 8),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 60.0, rep: 7),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 50.0, rep: 10),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 55.0, rep: 12),
+                    ]
+                )
+            ]
+        )
+        data.append(history1)
+        let history2 = TrainingHistory(
+            part: "脚",
+            trainingMenuList: [
+                TrainingHistory.TrainingMenu(
+                    trainingMenuName: "スクワット",
+                    trainingSetList: [
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 100.0, rep: 8),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 80.0, rep: 8),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 70.0, rep: 12),
+                        TrainingHistory.TrainingMenu.TrainingSet(weight: 60.0, rep: 10),
+                    ]
+                )
+            ]
+        )
+        data.append(history2)
     }
     
     private func configureNavBar() {
@@ -51,10 +100,23 @@ class HomeViewController: UIViewController, GetClassNameProtocol {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: TrainingHistoryTableViewCell.className) as? TrainingHistoryTableViewCell ?? {
+            assertionFailure("登録済みのセルが取得できない")
+            return TrainingHistoryTableViewCell()
+        }()
+
+        // セルに情報をセット
+        
+        
+        // トレーニングメニューごとに並び替え
+        let sortedListByTrainingMenu = data[indexPath.row].trainingMenuList.sorted { $0.trainingMenuName < $1.trainingMenuName }
+        print("ソートしたトレーニングリスト \(sortedListByTrainingMenu)")
+        
+        return cell
     }
 }
